d57d8b32443262530993533f857c8e5f8747ff3b
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-12 18:17:04 -0400

    Crea RegistroOcupacionesApp.kt

---
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/RegistroOcupacionesApp.kt
@@ -0,0 +1,9 @@
+package edu.ucne.registrodeocupacionesjesus
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class RegistroOcupacionesApp: Application() {
+}
