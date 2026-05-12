e89c2ee67b4ddbe4d96d1760c5358882840252d6
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-12 18:44:06 -0400

    Modifica DeleteOcupacionUseCase.kt y GetOcupacionUseCase.kt

--- app/src/main/java/edu/ucne/registrodeocupacionesjesus/domain/usecase/GetOcupacionUseCase.kt
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/domain/usecase/GetOcupacionUseCase.kt
@@ -1,8 +1,9 @@
 package edu.ucne.registrodeocupacionesjesus.domain.usecase
 
 import edu.ucne.registrodeocupacionesjesus.domain.repository.OcupacionRepository
+import javax.inject.Inject
 
-class GetOcupacionUseCase(
+class GetOcupacionUseCase @Inject constructor(
     private val repository: OcupacionRepository
 ) {
     suspend operator fun invoke(id: Int) = repository.getOcupacion(id)
