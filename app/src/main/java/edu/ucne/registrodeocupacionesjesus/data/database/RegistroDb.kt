2bfc5e2f59f68f90d23d0f371e5523a7ee172d2c
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-16 00:02:59 -0400

    Modifica RegistroDb.kt

--- app/src/main/java/edu/ucne/registrodeocupacionesjesus/data/database/RegistroDb.kt
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/data/database/RegistroDb.kt
@@ -2,6 +2,9 @@ package edu.ucne.registrodeocupacionesjesus.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import edu.ucne.registrodeocupacionesjesus.data.empleados.local.Converters
 import edu.ucne.registrodeocupacionesjesus.data.empleados.local.EmpleadoDao
 import edu.ucne.registrodeocupacionesjesus.data.empleados.local.EmpleadoEntity
 import edu.ucne.registrodeocupacionesjesus.data.ocupaciones.local.OcupacionDao
@@ -11,6 +14,7 @@ import edu.ucne.registrodeocupacionesjesus.data.ocupaciones.local.OcupacionEntit
     entities = [OcupacionEntity :: class, EmpleadoEntity :: class],
     version = 2
 )
+@TypeConverters(Converters::class)
 abstract class RegistroDb : RoomDatabase() {
     abstract fun OcupacionDao(): OcupacionDao
     abstract fun EmpleadoDao(): EmpleadoDao
