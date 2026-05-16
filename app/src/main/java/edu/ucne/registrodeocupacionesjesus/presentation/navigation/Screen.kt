dda231ece63a7a999becab72156d397af852329e
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-16 00:03:23 -0400

    Modifica Screen.kt

--- app/src/main/java/edu/ucne/registrodeocupacionesjesus/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/presentation/navigation/Screen.kt
@@ -8,4 +8,12 @@ sealed class Screen {
 
     @Serializable
     data class OcupacionEdit(val ocupacionId : Int = 0) : Screen()
+
+    @Serializable
+    data object EmpleadoList : Screen()
+
+    @Serializable
+    data class EmpleadoEdit(val ocupacionId : Int = 0) : Screen()
 }
+
+
