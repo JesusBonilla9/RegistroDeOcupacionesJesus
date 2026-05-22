86e612d1de67b10c91adb3fa601da4c5ce627211
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-22 10:27:36 -0400

    Modifica EmpleadoDao.kt

--- app/src/main/java/edu/ucne/registrodeocupacionesjesus/data/empleados/local/EmpleadoDao.kt
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/data/empleados/local/EmpleadoDao.kt
@@ -13,16 +13,16 @@ interface EmpleadoDao {
     @Delete
     suspend fun delete(entity: EmpleadoEntity)
 
-    @Query("Select * From Empleados ORDER BY empleadoId DESC")
+    @Query("Select * From empleados ORDER BY empleadoId DESC")
     fun observeAll(): Flow<List<EmpleadoEntity>>
 
-    @Query("Select * From Empleados WHERE empleadoId = :id")
+    @Query("Select * From empleados WHERE empleadoId = :id")
     suspend fun getById(id: Int): EmpleadoEntity?
 
-    @Query ("DELETE FROM Empleados WHERE empleadoId = :id")
+    @Query ("DELETE FROM empleados WHERE empleadoId = :id")
     suspend fun deleteById(id: Int)
 
-    @Query("Select exists(Select 1 From Empleados WHERE empleadoId = :id)")
+    @Query("Select exists(Select 1 From empleados WHERE empleadoId = :id)")
     suspend fun exists(id: Int): Boolean
 
 }
