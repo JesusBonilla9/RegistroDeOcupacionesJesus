c64df6c43f879723adb90f4e32deeb5a33b7f387
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-22 10:32:45 -0400

    Crea HoraExtraEntity y HoraExtraDao.kt

---
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/data/horasExtra/local/HoraExtraEntity.kt
@@ -0,0 +1,17 @@
+package edu.ucne.registrodeocupacionesjesus.data.horasExtra.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "horasExtra")
+data class HoraExtraEntity(
+    @PrimaryKey(autoGenerate = true)
+    val horaExtraId: Int = 0,
+    val empleadoId: Int = 0,
+    val fecha: LocalDate = LocalDate.now(),
+    val cantidadHoras : Int = 0,
+    val tipoHoraExtra: TipoHoraExtra,
+    val recargo: Double = 0.0,
+    val esPuestoDireccion: Boolean
+)
