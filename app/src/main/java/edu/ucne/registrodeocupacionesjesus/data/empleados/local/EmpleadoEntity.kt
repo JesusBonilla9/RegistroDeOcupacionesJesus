f1bf06e1667c1e28bb21eb998f7b9d0dc05c3568
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-22 10:28:31 -0400

    Modifica EmpleadoEntity.kt

--- app/src/main/java/edu/ucne/registrodeocupacionesjesus/data/empleados/local/EmpleadoEntity.kt
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/data/empleados/local/EmpleadoEntity.kt
@@ -4,13 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.LocalDate
 
-@Entity(tableName = "Empleados")
+@Entity(tableName = "empleados")
 data class EmpleadoEntity(
     @PrimaryKey(autoGenerate = true)
+    val ocupacionId: Int = 0,
     val empleadoId : Int = 0,
     val fechaIngreso : LocalDate = LocalDate.now(),
     val nombres : String = "",
     val sexo : String = "",
-    val sueldo : Double = 0.0
+    val sueldo : Double = 0.0,
+    val frecuenciaPago: FrecuenciaPago
 )
 
