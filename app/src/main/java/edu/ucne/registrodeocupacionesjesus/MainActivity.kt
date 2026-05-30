5852fc09a5312f807dd13cda12ee3018a8910778
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-05-30 19:21:52 -0400

    Quita imports innecesarios y quita un data class innecesario de MainActivity.kt

--- app/src/main/java/edu/ucne/registrodeocupacionesjesus/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrodeocupacionesjesus/MainActivity.kt
@@ -4,26 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccessTime
-import androidx.compose.material.icons.filled.People
-import androidx.compose.material.icons.filled.Work
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavDestination.Companion.hasRoute
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.registrodeocupacionesjesus.presentation.navigation.MainNavigationHost
-import edu.ucne.registrodeocupacionesjesus.presentation.navigation.Screen
 import edu.ucne.registrodeocupacionesjesus.ui.theme.RegistroDeOcupacionesJesusTheme
 
 
@@ -40,9 +21,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class TopLevelRoute<T : Any>(
-    val nombre: String,
-    val ruta: T,
-    val icono: androidx.compose.ui.graphics.vector.ImageVector
-)
 
